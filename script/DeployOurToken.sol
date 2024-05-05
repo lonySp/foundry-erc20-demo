2ee4c6f204227c2950e4c0b86591f4bf77482247
xiaoyuan <lony0116@163.com>
2024-05-05 14:16:04 +0800

    modules

---
+++ script/DeployOurToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+
+import {Script} from "forge-std/Script.sol";
+import {OurToken} from "../src/OurToken.sol";
+
+contract DeployOurToken is Script {
+
+    uint256 public constant INITIAL_SUPPLY = 1000000 ether;
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+        new OurToken(INITIAL_SUPPLY);
+        vm.stopBroadcast();
+    }
+}
